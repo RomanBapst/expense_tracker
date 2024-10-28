4545de0de61faca016c55e5e4c7c395bafc2ed92
Roman Bapst <bapstroman@gmail.com>
2024-10-28 07:39:18 +0300

    shorten the version string
    Signed-off-by: Roman Bapst <bapstroman@gmail.com>

--- frontend/src/components/ExpensePage.vue
+++ frontend/src/components/ExpensePage.vue
@@ -2,7 +2,7 @@
   <NavigationBar />
   <div class="container mx-auto mt-8">
     <fwb-heading tag="h1" class="text-blue-400 mb-6 text-2xl font-bold"
-    >Expense Tracker Version: ({{ gitHash }})</fwb-heading
+    >Expense Tracker Ver: ({{ gitHash }})</fwb-heading
     >
     <!-- Updated title style -->
     
