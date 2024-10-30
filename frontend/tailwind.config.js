01e7cc39e4aeb02b5041788e33cd03ae6f5b4832
Roman Bapst <bapstroman@gmail.com>
2024-10-30 23:02:40 +0300

    enable tailwind
    Signed-off-by: Roman Bapst <bapstroman@gmail.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './src/**/*.{html,js,vue}',
     'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
     'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
   ],
