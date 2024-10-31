31ede79b0114a4c19180cfd572fcd87ccb0dc79a
Roman Bapst <bapstroman@gmail.com>
2024-10-31 15:56:50 +0300

    fixed empty table when no sorting function is provided
    Signed-off-by: Roman Bapst <bapstroman@gmail.com>

--- frontend/src/components/SimpleTable.vue
+++ frontend/src/components/SimpleTable.vue
@@ -34,29 +34,34 @@ onMounted(() => {
 });
 
 watch(
-  () => props.items,
-  (_newValue) => {
+() => props.items,
+(newValue) => {
+  
+  if (!props.sortFunction) {
+    sortedItems.value = newValue
+  } else {
     sortDirection.value = sortDirection.value === "asc" ? "desc" : "asc";
     sortByColumn(lastSortIndex.value)
   }
+}
 );
 
 function sortByColumn(columnIndex) {
   if (!props.sortFunction) {
     return;
   }
-
+  
   lastSortIndex.value = columnIndex
-
+  
   if (sortedColumn.value === columnIndex) {
     sortDirection.value = sortDirection.value === "asc" ? "desc" : "asc";
   } else {
     sortedColumn.value = columnIndex;
     sortDirection.value = "asc";
   }
-
+  
   sortedItems.value = props.sortFunction(columnIndex, sortDirection.value, [
-    ...props.items,
+  ...props.items,
   ]);
 }
 </script>
@@ -65,47 +70,47 @@ function sortByColumn(columnIndex) {
   <fwb-table class="overflow-y-auto border border-gray-300">
     <fwb-table-head>
       <fwb-table-head-cell
-        v-for="(name, index) in header"
-        :key="name"
-        @click="sortByColumn(index)"
-        class="cursor-pointer text-left px-4 py-2 font-semibold text-gray-700"
+      v-for="(name, index) in header"
+      :key="name"
+      @click="sortByColumn(index)"
+      class="cursor-pointer text-left px-4 py-2 font-semibold text-gray-700"
       >
-        {{ name }}
-        <span
-          v-if="sortedColumn === index"
-          :class="{
-            'icon-arrow-up': sortDirection === 'asc',
-            'icon-arrow-down': sortDirection === 'desc',
-          }"
-        ></span>
-      </fwb-table-head-cell>
-      <fwb-table-head-cell>
-        <span class="sr-only">Actions</span>
-      </fwb-table-head-cell>
-    </fwb-table-head>
-    <fwb-table-body>
-      <fwb-table-row v-for="item in sortedItems" :key="item.id">
-        <template #default>
-          <fwb-table-cell
-            v-for="(value, index) in item.values"
-            :key="index"
-            class="px-4 py-2"
+      {{ name }}
+      <span
+      v-if="sortedColumn === index"
+      :class="{
+        'icon-arrow-up': sortDirection === 'asc',
+        'icon-arrow-down': sortDirection === 'desc',
+      }"
+      ></span>
+    </fwb-table-head-cell>
+    <fwb-table-head-cell>
+      <span class="sr-only">Actions</span>
+    </fwb-table-head-cell>
+  </fwb-table-head>
+  <fwb-table-body>
+    <fwb-table-row v-for="item in sortedItems" :key="item.id">
+      <template #default>
+        <fwb-table-cell
+        v-for="(value, index) in item.values"
+        :key="index"
+        class="px-4 py-2"
+        >
+        <slot :name="'cell-' + index" :item="item">
+          <div
+          class="max-w-xs overflow-hidden text-ellipsis whitespace-nowrap"
+          :title="value"
           >
-            <slot :name="'cell-' + index" :item="item">
-              <div
-                class="max-w-xs overflow-hidden text-ellipsis whitespace-nowrap"
-                :title="value"
-              >
-                {{ value }}
-              </div>
-            </slot>
-          </fwb-table-cell>
-          <fwb-table-cell class="flex justify-end px-4 py-2">
-            <slot name="button1" :emit="$emit" :item="item"></slot>
-            <slot name="button2" :emit="$emit" :item="item"></slot>
-          </fwb-table-cell>
-        </template>
-      </fwb-table-row>
-    </fwb-table-body>
-  </fwb-table>
+          {{ value }}
+        </div>
+      </slot>
+    </fwb-table-cell>
+    <fwb-table-cell class="flex justify-end px-4 py-2">
+      <slot name="button1" :emit="$emit" :item="item"></slot>
+      <slot name="button2" :emit="$emit" :item="item"></slot>
+    </fwb-table-cell>
+  </template>
+</fwb-table-row>
+</fwb-table-body>
+</fwb-table>
 </template>
